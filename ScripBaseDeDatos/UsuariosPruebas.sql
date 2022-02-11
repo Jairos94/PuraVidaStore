40d8de48c207ba4de720521644a1e576fbc4ee4c
Jairo Rivera <jairo.ri.ce@gmail.com>
2022-02-10 21:46:12 -0600

    Encriptado sql server

---
+++ ScripBaseDeDatos/UsuariosPruebas.sql
@@ -0,0 +1,20 @@
+USE [PuraVidaStore]
+GO
+
+INSERT INTO [dbo].[Persona]
+           ([PsrIdentificacion]
+           ,[PsrNombre]
+           ,[PsrApellido1]
+           ,[PsrApellido2])
+     VALUES
+           ('1'
+           ,'Administrador'
+           ,''
+           ,''),
+		   ('2'
+           ,'Vendedor'
+           ,''
+           ,'')
+GO
+
+
