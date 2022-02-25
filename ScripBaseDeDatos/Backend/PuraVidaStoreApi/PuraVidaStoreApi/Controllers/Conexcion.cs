2dcf32c20c9ebc312e752e9bfa00c87d803a569b
Jairo Rivera <jairo.ri.ce@gmail.com>
2022-02-24 20:31:32 -0600

    Cambios visuales de angular

--- ScripBaseDeDatos/Backend/PuraVidaStoreApi/PuraVidaStoreApi/Controllers/Conexcion.cs
+++ ScripBaseDeDatos/Backend/PuraVidaStoreApi/PuraVidaStoreApi/Controllers/Conexcion.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
+
 
 namespace PuraVidaStoreApi.Model
 {
     public class Conexcion
     {
         private string server= "DESKTOP-4FJOI9V";
+        private string Database= "PuraVidaStore";
         private string user= "pvs";
         private string pass= "$psv2022$";
-        private string Database= "PuraVidaStore";
-
-        public string CadenaString1 { get; set; } = "Data Source=" & server & ";Initial Catalog=" + Database + ";Persist Security Info=True;User ID=" + user + ";Password=" + pass;
+        
 
         public string caneda() 
         {
-            return CadenaString1;
+            string CadenaString = "Data Source=" + server + ";Initial Catalog=" + Database + ";Persist Security Info=True;User ID=" + user + ";Password=" + pass;
+            return CadenaString;
         }
     }
 }
--- ScripBaseDeDatos/FrontEnd/PuraVidaStorCR/src/app/app.module.ts
+++ ScripBaseDeDatos/FrontEnd/PuraVidaStorCR/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+//Primereng
+import {AccordionModule} from 'primeng/accordion'; 
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule
+  ],
+  exports: [
+    LoginComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
