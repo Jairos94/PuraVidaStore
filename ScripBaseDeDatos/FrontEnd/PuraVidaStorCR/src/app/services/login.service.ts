6caea297aa0b8bcd4b91bc93ecaa16a9341db148
Jairos94 <jairo.ri.ce@gmail.com>
2022-05-09 20:39:14 -0600

    PrimerCommi

--- ScripBaseDeDatos/FrontEnd/PuraVidaStorCR/src/app/services/login.service.ts
+++ ScripBaseDeDatos/FrontEnd/PuraVidaStorCR/src/app/services/login.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { rutaGeneral } from './ruta.interface';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private ruta:rutaGeneral) { }
+
+
+
+  
+  get httpParams() {
+    return new HttpParams().set('fields', 'user,password')
+  }
+
+
+  buscarUsuario(_Usuario:string,_pass:string,termino:string)
+  {
+    /onst url = `${this.ruta?.RutaGeneral}/${termino}`;
+  }
 }
---
+++ ScripBaseDeDatos/FrontEnd/PuraVidaStorCR/src/app/services/ruta.interface.ts
@@ -0,0 +1,4 @@
+export interface rutaGeneral
+{
+    RutaGeneral:'https://localhost:7089/';
+}
