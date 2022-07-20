9ebc5eb308827a06f74cd54203e1b19ed47c6a42
Jairos94 <jairo.ri.ce@gmail.com>
2022-07-19 19:41:35 -0600

    Cambios

--- ScripBaseDeDatos/FrontEnd/FrontEndPuraVidaStore/src/app/app-routing.module.ts
+++ ScripBaseDeDatos/FrontEnd/FrontEndPuraVidaStore/src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { PrincipalComponent } from './principal/principal.component';
 
 const routes: Routes = [
 
   { path: 'login', component: LoginComponent },
+ 
   {
     path: 'principal',
-    component: PrincipalComponent,
     loadChildren: () => import('./principal/principal.module').then(m => m.PrincipalModule)
   },
+  
   { path: '', redirectTo: 'login', pathMatch: 'full' }, // redirect to `first-component`
   { path: '**', redirectTo: 'login'},  // Wildcard route for a 404 page
 ];
--- ScripBaseDeDatos/FrontEnd/FrontEndPuraVidaStore/src/app/principal/componentes/componentes.component.ts
+++ ScripBaseDeDatos/FrontEnd/FrontEndPuraVidaStore/src/app/principal/componentes/componentes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api/menuitem';
+import { activo } from 'src/app/activo';
 
 @Component({
   selector: 'app-componentes',
@@ -8,8 +10,36 @@ import { Component, OnInit } from '@angular/core';
 export class ComponentesComponent implements OnInit {
 
   constructor() { }
+  
+  usuario: string = '';
+  items: MenuItem[] = [];
+  administrador: boolean = false;
 
   ngOnInit(): void {
+    this.usuario = activo.usuarioPrograma.usuario;
+    if (activo.usuarioPrograma.idRol === 1) {
+      this.administrador = true;
+      this.items = [
+        {
+          label: 'ventas',
+          icon: 'pi pi-money-bill',
+          routerLink: "./ventas",
+        },
+
+        {
+          label: 'Usuarios',
+          icon: 'pi pi-users',
+          routerLink: "./usuarios",
+        },
+      ];
+    } else {
+      this.administrador = false;
+      this.items = [
+        {
+          label: 'Edit',
+        }
+      ];
+    }
   }
 
 }
