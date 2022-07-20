04d424086ae9b252ed6a6f1b957d3ed1241d384b
Jairos94 <jairo.ri.ce@gmail.com>
2022-07-19 22:33:18 -0600

    Cambios

---
+++ ScripBaseDeDatos/FrontEnd/puraVidaStoreFE/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: 'principal', loadChildren: () => import('./principal/principal.module').then(m => m.PrincipalModule) }, 
+  {path:'**',redirectTo:'login'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
