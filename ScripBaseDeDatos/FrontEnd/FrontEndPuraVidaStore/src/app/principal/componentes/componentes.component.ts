663652814806c3142aa45f6be357b400ee5d623d
Jairos94 <jairo.ri.ce@gmail.com>
2022-07-19 20:14:18 -0600

    Cambios

--- ScripBaseDeDatos/FrontEnd/FrontEndPuraVidaStore/src/app/principal/componentes/componentes.component.ts
+++ ScripBaseDeDatos/FrontEnd/FrontEndPuraVidaStore/src/app/principal/componentes/componentes.component.ts
@@ -10,36 +10,9 @@ import { activo } from 'src/app/activo';
 export class ComponentesComponent implements OnInit {
 
   constructor() { }
-  
-  usuario: string = '';
-  items: MenuItem[] = [];
-  administrador: boolean = false;
 
   ngOnInit(): void {
-    this.usuario = activo.usuarioPrograma.usuario;
-    if (activo.usuarioPrograma.idRol === 1) {
-      this.administrador = true;
-      this.items = [
-        {
-          label: 'ventas',
-          icon: 'pi pi-money-bill',
-          routerLink: "./ventas",
-        },
-
-        {
-          label: 'Usuarios',
-          icon: 'pi pi-users',
-          routerLink: "./usuarios",
-        },
-      ];
-    } else {
-      this.administrador = false;
-      this.items = [
-        {
-          label: 'Edit',
-        }
-      ];
-    }
+    
   }
 
 }
