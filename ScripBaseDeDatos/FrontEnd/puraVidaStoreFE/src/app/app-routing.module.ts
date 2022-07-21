340571937b5aa9d2a6c394cb1a78355b5e11126c
Jairos94 <jairo.ri.ce@gmail.com>
2022-07-20 19:59:13 -0600

    no message

--- ScripBaseDeDatos/FrontEnd/puraVidaStoreFE/src/app/app-routing.module.ts
+++ ScripBaseDeDatos/FrontEnd/puraVidaStoreFE/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   { path: 'principal', loadChildren: () => import('./principal/principal.module').then(m => m.PrincipalModule) },
+  { path: 'usuarios', loadChildren: () => import('./principal/usuarios/usuarios.module').then(m => m.UsuariosModule) },
+  { path: 'ventas', loadChildren: () => import('./principal/ventas/ventas.module').then(m => m.VentasModule) },
   {path:'**',redirectTo:'login'}
 ];
 
