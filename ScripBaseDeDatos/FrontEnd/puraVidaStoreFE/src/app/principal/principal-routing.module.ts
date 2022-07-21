72e60b40c186db781bbfe6bc649bf12658e7103a
Jairos94 <jairo.ri.ce@gmail.com>
2022-07-20 19:59:13 -0600

    no message

--- ScripBaseDeDatos/FrontEnd/puraVidaStoreFE/src/app/principal/principal-routing.module.ts
+++ ScripBaseDeDatos/FrontEnd/puraVidaStoreFE/src/app/principal/principal-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './principal.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { VentasComponent } from './ventas/ventas.component';
 
 const routes: Routes = [
-  { path: '', component: PrincipalComponent },
-  
-  { path: 'usuarios', loadChildren: () => import('../principal/usuarios/usuarios.module').then(m => m.UsuariosModule) },
-  { path: 'ventas', loadChildren: () => import('../principal/ventas/ventas.module').then(m => m.VentasModule) },
+  { 
+    path: '', 
+    component: PrincipalComponent,
+    children:[
+      {path:'usuarios',component:UsuariosComponent},
+      {path:'ventas',component:VentasComponent},
+
+      
+      {path:'',redirectTo:'ventas'}
+    ]
+  },
 ];
 
 @NgModule({
