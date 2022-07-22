f805ad8595d21d4b93ed4f04b8908be671338bd4
Jairos94 <jairo.ri.ce@gmail.com>
2022-07-21 18:12:47 -0600

    no message

--- ScripBaseDeDatos/FrontEnd/FrontEndPuraVidaStore/src/app/app-routing.module.ts
+++ ScripBaseDeDatos/FrontEnd/FrontEndPuraVidaStore/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
 
   { path: 'login', component: LoginComponent },
   
-  { path: '**', redirectTo:'login', pathMatch:'full'}
+  { path: '**', redirectTo:'login'},
 ];
 
 @NgModule({
