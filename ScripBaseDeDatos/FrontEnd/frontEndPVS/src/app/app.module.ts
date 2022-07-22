afd076efa5b1a236b87f1105748de91f5794407d
Jairos94 <jairo.ri.ce@gmail.com>
2022-07-21 19:00:23 -0600

    no message

--- ScripBaseDeDatos/FrontEnd/frontEndPVS/src/app/app.module.ts
+++ ScripBaseDeDatos/FrontEnd/frontEndPVS/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrimeNgModule } from './prime-ng.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    PrimeNgModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ScripBaseDeDatos/FrontEnd/frontEndPVS/src/app/models/usuarios-models.ts
@@ -0,0 +1,2 @@
+export interface UsuariosModels {
+}
