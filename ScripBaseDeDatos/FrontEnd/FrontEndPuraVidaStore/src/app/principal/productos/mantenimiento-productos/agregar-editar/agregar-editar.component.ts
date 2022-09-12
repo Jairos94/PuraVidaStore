91adaee5f621620b6c11532700c9c58057e1316b
Jairos94 <jairo.ri.ce@gmail.com>
2022-09-11 20:59:52 -0600

    Cambios se agrega foto, ya se captura imagen en variable

--- ScripBaseDeDatos/FrontEnd/FrontEndPuraVidaStore/src/app/principal/productos/mantenimiento-productos/agregar-editar/agregar-editar.component.ts
+++ ScripBaseDeDatos/FrontEnd/FrontEndPuraVidaStore/src/app/principal/productos/mantenimiento-productos/agregar-editar/agregar-editar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductoModel } from 'src/app/models/producto-model';
 import { TipoProductoModel } from 'src/app/models/tipo-producto';
 import { TipoProductoService } from 'src/app/services/tipo-producto.service';
 
@@ -10,22 +11,37 @@ import { TipoProductoService } from 'src/app/services/tipo-producto.service';
 export class AgregarEditarComponent implements OnInit {
 
   listaTipoProductos: TipoProductoModel[] = []
-  private archivoTemporal: any;
-  constructor(private servicioTipoProducto:TipoProductoService) { }
+  productoEditarAgregar: ProductoModel =
+    {
+      prdId: 0,
+      prdNombre:'',
+      prdPrecioVentaMayorista:0,
+      prdPrecioVentaMinorista:0,
+      prdCodigo:'',
+      prdUnidadesMinimas:0,
+      prdIdTipoProducto:0,
+      prdCodigoProvedor:'',
+      prdFoto:null,
+      pdrVisible:true
+    };
+  constructor(private servicioTipoProducto: TipoProductoService) { }
 
   ngOnInit(): void {
     this.listaTipoProductoFiltrado();
   }
 
   archivo(evento: any) {
-    const [file] = evento.currentFiles;
-    console.log(file);
+    const archivo = <File>evento.currentFiles[0];
+
+    //Metodo para colocar en variable
+    this.productoEditarAgregar.prdFoto = new FormData();
+    this.productoEditarAgregar.prdFoto.append('imagen', archivo, archivo.name);
+
   }
 
-  listaTipoProductoFiltrado(){
-    this.servicioTipoProducto.listaTipoProductoFiltrado().subscribe((tp=>{
-    this.listaTipoProductos = tp;
-    console.log(this.listaTipoProductos);
-    }),(_e=>console.log(_e)));
+  listaTipoProductoFiltrado() {
+    this.servicioTipoProducto.listaTipoProductoFiltrado().subscribe((tp => {
+      this.listaTipoProductos = tp;
+    }), (_e => console.log(_e)));
   }
 }
