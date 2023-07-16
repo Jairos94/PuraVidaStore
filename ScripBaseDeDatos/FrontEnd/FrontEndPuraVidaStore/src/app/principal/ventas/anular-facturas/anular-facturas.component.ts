0210ebf8a5e957b2fb087ecbd6af35043d2f1283
Jairos94 <jairo.ri.ce@gmail.com>
2023-07-15 20:23:25 -0600

    Cambios

---
+++ ScripBaseDeDatos/FrontEnd/FrontEndPuraVidaStore/src/app/principal/ventas/anular-facturas/anular-facturas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anular-facturas',
+  templateUrl: './anular-facturas.component.html',
+  styleUrls: ['./anular-facturas.component.css']
+})
+export class AnularFacturasComponent {
+
+}
